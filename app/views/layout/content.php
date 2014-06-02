3ca86166e8698c347b6cd202720e727448b18979
Dionysis Fryganas <dfryganas@gmail.com>
2014-06-02 03:51:12 +0200

    Changed the sites layout.

--- app/views/layout/content.php
+++ app/views/layout/content.php
@@ -1,7 +1,22 @@
-<?php global $www, $view; ?>
-<div id="content">
-<h3><?php /* echo $view->$pageHeader; */ ?>Fancy</h3>
-<div id="content-main">
-<?php include_once $view->file; ?>
-</div>
+<?php global $www, $view, $error; ?>
+
+<div id="container">
+	<div id="header"></div>
+	<div class="col-md-2">
+			<p></p>
+		</div>
+
+	<div class="row">
+		<div class="col-md-8">
+			<h1>Welcome to the Corporate Wiki!</h1>
+			<div class="row">
+				<p><?php echo $error->printErrorMsg(); ?></p>
+            <?php include_once $view->file; ?>
+            </div>
+		</div>
+
+	</div>
+	<div class="col-md-2">
+			<p></p>
+		</div>
 </div>
--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -1,10 +1,9 @@
-
+<div id="footer">
+	<script
+		src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script type="text/javascript"
+		src="<?php echo $www['scripts'].'bootstrap.js';?>"></script>
 </div>
-<?php
-/*
- * // @fix: Inlude jquery | Bootstrap requires jquery.
- * <script type="text/javascript" src="<?php echo $www['scripts'].'bootstrap.js';?>"></script>
- */
-?>
+</div>
 </body>
 </html>
