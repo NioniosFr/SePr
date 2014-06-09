4519a8dacdf3afd148fc61746940f0c8788a7b9d
Dionysis Fryganas <dfryganas@gmail.com>
2014-06-10 01:56:15 +0200

    Added a session security table.

--- DB_script/db_create.sql
+++ DB_script/db_create.sql
@@ -2,9 +2,18 @@
 
 CREATE DATABASE IF NOT EXISTS sepr;
 
+-- Create user Section --------------------------------------------------
+
+CREATE User ''@'%' IDENTIFIED BY '';
+
+GRANT ALL PRIVILEGES on sepr.* to ''@'%';
+
+FLUSH PRIVILEGES;
+
 -- drop table section -----------------------------------------------------------
 
 DROP TABLE IF EXISTS sepr.user_auth;
+DROP TABLE IF EXISTS sepr.session_otun;
 
 -- create table section ---------------------------------------------------------
 
@@ -16,7 +25,18 @@ CREATE TABLE IF NOT EXISTS sepr.user_auth
   secure_question Char(255) NOT NULL,
   secure_answer Char(255) NOT NULL,
   PRIMARY KEY (email_address),
-  UNIQUE email_address (email_address)
+  UNIQUE email_address (email_address),
+  UNIQUE usera_name (user_name)
+);
+
+-- create table otun ---one time use number----------------
+
+CREATE TABLE IF NOT EXISTS sepr.session_otun
+(
+  otun Int NOT NULL,
+  user_name Char(50) NOT NULL,
+  PRIMARY KEY (otun),
+  UNIQUE otun (otun)
 );
 
 -- drop trigger section ---------------------------------------------------------
@@ -47,6 +67,16 @@ IF (NEW.secure_answer IS NOT NULL) THEN
 END |
 delimiter;
 
+-- Realtionships ------------------------
+
+ALTER TABLE sepr.session_otun ADD 
+CONSTRAINT user_auth_user_name_fk 
+FOREIGN KEY (user_name) 
+REFERENCES sepr.user_auth (user_name) 
+ON DELETE NO ACTION 
+ON UPDATE NO ACTION
+;
+
 -- populate section -------------------------------------------------------------
 
 INSERT INTO sepr.user_auth (email_address,user_name,passw, secure_question,secure_answer) VALUES 
@@ -56,7 +86,6 @@ INSERT INTO sepr.user_auth (email_address,user_name,passw, secure_question,secur
 -- TESTING ----------------------------------------------------------------------
 
 select * from sepr.user_auth where passw = sha2('password_testing',256);
-
 
 show triggers from sepr;
 
