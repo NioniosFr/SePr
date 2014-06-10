948ad2f0bec9d0f82b802bc82f480e4bc9cfcc39
Dionysis Fryganas <dfryganas@gmail.com>
2014-06-10 06:33:27 +0200

    Removed unnecesary code..

--- app/views/default/index.php
+++ app/views/default/index.php
@@ -1,9 +1,5 @@
 <?php
 if (! defined('CW'))
     exit('invalid access');
-
-global $db;
-
-$result = $db->select(sprintf("select * from user_auth where `passw` = sha2('%s',256)", 'password_testing'));
 ?>
 <h2>Default view</h2>
