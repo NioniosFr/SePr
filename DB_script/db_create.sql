0039235ad5d3e48e9c6432f5d6d919cce6908ca8
Dionysis Fryganas <dfryganas@gmail.com>
2014-06-14 03:07:36 +0200

    Added population variables.

--- DB_script/db_create.sql
+++ DB_script/db_create.sql
@@ -26,17 +26,40 @@ CREATE TABLE IF NOT EXISTS sepr.user_auth
   secure_answer Char(255) NOT NULL,
   PRIMARY KEY (email_address),
   UNIQUE email_address (email_address),
-  UNIQUE user_name (user_name)
+  UNIQUE usera_name (user_name)
+);
+
+-- create table user permissions ------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `sepr.permissions` (
+  `user_name` char(25) NOT NULL,
+  `create` tinyint(1) DEFAULT '0',
+  `read` tinyint(1) DEFAULT '0',
+  `update` tinyint(1) DEFAULT '0',
+  `delete` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`user_name`)
 );
 
 -- create table otun ---one time use number----------------
 
-CREATE TABLE IF NOT EXISTS sepr.session_otun
-(
-  otun Integer NOT NULL,
-  user_name Char(50) NOT NULL,
-  PRIMARY KEY (otun),
-  UNIQUE otun (otun)
+CREATE TABLE IF NOT EXISTS sepr.otun (
+user_name Char(25) NOT NULL,
+otun Int NOT NULL UNIQUE,
+created  datetime not null,
+modified TIMESTAMP DEFAULT now(),
+PRIMARY KEY (user_name),
+UNIQUE otun (otun)
+);
+
+-- crete page table -----
+
+CREATE TABLE IF NOT EXISTS sepr.page(
+`page_id` Int NOT NULL AUTO_INCREMENT,
+`last_edited_by` char(50) NOT NULL,
+`created` datetime NOT NULL,
+`modified` TIMESTAMP NOT NULL DEFAULT now(),
+`text` text,
+PRIMARY KEY (`page_id`)
 );
 
 -- drop trigger section ---------------------------------------------------------
@@ -95,9 +118,21 @@ ON UPDATE NO ACTION
 
 -- populate section -------------------------------------------------------------
 
-INSERT INTO sepr.user_auth (email_address,user_name,passw, secure_question,secure_answer) VALUES 
-('user1@email.com','user1','password_testing','What are we doing?','testing'),
-('user2@email.com','user2','password_testing','What are we doing?','testing');
+INSERT INTO sepr.user_auth (email_address,user_name,passw, secure_question,secure_answer) VALUES
+('admin','administrator','admin','What are we doing?','testing');
+('anonymous','anonymous','anonymous','What are we doing?','testing');
+('user1@email.com','user1','user1','What are we doing?','testing'),
+('user2@email.com','user2','user2','What are we doing?','testing');
+
+INSERT INTO sepr.permissions (`user_name`,`create`, `read`, `update`, `delete`) VALUES
+('administrator', 1,1,1,1),
+('anonymous', 0,1,0,0),
+('user1',0,1,1,0),
+('user2',1,1,1,0);
+
+INSERT INTO sepr.page (last_edited_by,created,text) VALUES
+('user1',now(),'This is a demo page content.'),
+('user2',now(),'This is another page content demo.');
 
 -- TESTING ----------------------------------------------------------------------
 
