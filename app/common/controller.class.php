9462ae4650ec39a2adc42340967018010af76b4c
Dionysis Fryganas <dfryganas@gmail.com>
2014-06-15 19:00:33 +0200

    Controller: Added comments for the interface.

--- app/common/controller.class.php
+++ app/common/controller.class.php
@@ -2,8 +2,26 @@
 if (! defined('CW'))
     exit('invalid access');
 
+/**
+ * The controller interface.
+ *
+ * Every controller should implement this interface.
+ *
+ * @author nionios
+ *
+ */
 interface Controller
 {
+
+    /**
+     * Intented to initialize the model and view classes for internal usage.
+     *
+     * @param array $args
+     */
     public function init($args);
+
+    /**
+     * The default action of every controller.
+     */
     public function index();
 }
