84a37e2ada6cdc9ff5503f7f15fb75cd80047048
Dionysis Fryganas <dfryganas@gmail.com>
2014-06-20 00:23:56 +0200

    Index: Changed route usage, added comments.

--- app/www/index.php
+++ app/www/index.php
@@ -1,19 +1,49 @@
 <?php
+/**
+ * The only directly accessible file of this application.
+ * Starts the application and serves the user requests.
+ */
+
+/**
+ * Security variable.
+ * Has to be checked at the top of every file to avoid direct access.
+ *
+ * @var boolean
+ */
 define('CW', 1); // CW stands for : Corporate Wiki.
 if (! defined('DS')) {
+    /**
+     * Assigns a smaller identifier for the PHP built in DIRECTORY_SEPERATOR.
+     *
+     * @var string
+     */
     define('DS', DIRECTORY_SEPARATOR);
 }
-// Define static paths.
+/**
+ * The file path to the root of this application.
+ *
+ * @var string A file path
+ */
 define('BASE', dirname(__DIR__) . DS);
+/**
+ * The file path to the folder containing the configuration files.
+ *
+ * @var string A file path
+ */
 define('ETC', BASE . 'etc' . DS);
+
 // Define the global variables.
-global $path, $www, $db, $session, $view, $route;
+global $path, $www, $db, $session, $view;
 /* Include global functions and classes. */
 require_once ETC . 'bootstrap.php';
 
+$route = get_route();
 $route->determineRequest();
+$route->executeController();
 $route->finalCheck();
+
 $view->render();
+
 exit();
 
 // Generally speaking:
